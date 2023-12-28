22e5ffefb87a2c37e0ef69c309d61559a0a9a5cf
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-12-28 15:52:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import './App.css';
 
-function App() {
+const InvoiceExtractor = () => {
+  const [invoiceImage, setInvoiceImage] = useState(null);
+  const [extractedText, setExtractedText] = useState('');
+  const [inputPrompt, setInputPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+const API_KEY = 'AIzaSyBpUjTpTatEQa_t7e8H4nRnSuqwKDsFAZQ';
+  const handleImageUpload = (event) => {
+    setInvoiceImage(event.target.files[0]);
+  };
+  
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    setError(null);
+    setExtractedText('');
+
+    try {
+      const genAI = new GoogleGenerativeAI(API_KEY);  
+      const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
+
+      const invoiceImagePart = await fileToGenerativePart(invoiceImage, 'image/jpeg');
+
+      const prompt = `
+        ${inputPrompt}
+        You are an expert in understanding invoices.
+        You will receive input images as invoices &
+        you will have to answer questions based on the input image
+      `;
+
+      const result = await model.generateContent([prompt, invoiceImagePart]);
+      const response = result.response;
+      const text = response.text();
+
+      setExtractedText(text);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fileToGenerativePart = (file, mimeType) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        resolve({
+          inlineData: {
+            data: reader.result.split(',')[1],
+            mimeType,
+          },
+        });
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container'>
+        <div className='left-column'>
+          <h1>Invoice Extractor</h1>
+
+          <div className='inputprompt'>
+            <label htmlFor="input-prompt">Input Prompt:</label>
+            <input type="text" id="input-prompt" value={inputPrompt} onChange={(e) => setInputPrompt(e.target.value)} />
+          </div>
+
+          <div className='extractedarea'>
+            <label htmlFor="extracted-text">Extracted Text:</label>
+            <p id="extracted-text">{extractedText} </p>
+          </div>
+
+          <button onClick={handleSubmit} disabled={isLoading}>
+            {isLoading ? 'Extracting...' : 'Tell me about the image'}
+          </button>
+        </div>
+
+        <div className='right-column'>
+          <div className='upload'>
+            <label htmlFor="invoice-image">Upload Invoice Image:</label>
+            <input type="file" id="invoice-image" accept="image/*" onChange={handleImageUpload} />
+          </div>
+
+          {invoiceImage && (
+            <div className='imagearea'>
+              <img src={URL.createObjectURL(invoiceImage)} alt="Uploaded invoice image" />
+            </div>
+          )}
+
+          {isLoading && <p>Extracting invoice...</p>}
+          {error && <p>Error: {error.message}</p>}
+        </div>
+      </div>
     </div>
   );
-}
 
-export default App;
+};
+
+export default InvoiceExtractor;
